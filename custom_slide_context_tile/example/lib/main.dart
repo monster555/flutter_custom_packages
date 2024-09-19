3e58ed3fe02adbbdd1e3c022aad4a9ea63d507aa
Daniel Coyula <monsterdrummer5@gmail.com>
2024-09-19 14:04:20 -0500

    feat: update the example with the new parameters

--- custom_slide_context_tile/example/lib/main.dart
+++ custom_slide_context_tile/example/lib/main.dart
@@ -32,11 +32,8 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  void logAction(String action) {
-    log(action, name: 'action');
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  void logAction(String action) => log(action, name: 'action');
 
   late CustomSlidableController controller;
   late SlidableManager manager;
@@ -50,33 +47,29 @@ class _MyHomePageState extends State<MyHomePage>
 
   List<MenuAction> get leadingActions => [
         MenuAction(
-          onPressed: () {},
+          onPressed: () => logAction('Home'),
           icon: Icons.home,
           label: 'Home',
           backgroundColor: Colors.green,
         ),
         MenuAction(
           icon: Icons.delete,
-          onPressed: () {
-            // Action when button is pressed
-          },
+          // Action when button is pressed
+          onPressed: () => logAction('Delete'),
           label: 'Delete',
           backgroundColor: Colors.blue,
         ),
         MenuAction(
           icon: Icons.home,
-          onPressed: () {
-            // Action when button is pressed
-          },
+          // Action when button is pressed
+          onPressed: () => logAction('Home'),
           label: 'Replay',
           backgroundColor: Colors.yellow,
         ),
         MenuAction(
           icon: Icons.delete,
-          onPressed: () {
-            // Action when button is pressed
-            log('Leading action executed');
-          },
+          // Action when button is pressed
+          onPressed: () => log('Leading action executed'),
           label: 'Delete',
           backgroundColor: Colors.red,
         ),
@@ -85,37 +78,30 @@ class _MyHomePageState extends State<MyHomePage>
   List<MenuAction> get trailingActions => [
         MenuAction(
           icon: Icons.person,
-          onPressed: () {
-            // Action when button is pressed
-            controller.openLeading();
-          },
+          // Action when button is pressed
+          onPressed: () => logAction('Person'),
           label: 'Person',
           backgroundColor: Colors.green,
         ),
         MenuAction(
           icon: Icons.copy,
-          onPressed: () {
-            // Action when button is pressed
-          },
+          // Action when button is pressed
+          onPressed: () => logAction('Copy'),
           label: 'Copy',
           backgroundColor: Colors.blue,
         ),
         MenuAction(
           icon: Icons.replay,
-          onPressed: () {
-            // Action when button is pressed
-            logAction('Replay');
-          },
+          // Action when button is pressed
+          onPressed: () => logAction('Replay'),
           label: 'Replay',
           backgroundColor: Colors.yellow,
         ),
         MenuAction(
           icon: Icons.delete,
-          onPressed: () {
-            // Action when button is pressed
-            log('Trailing action executed');
-          },
-          // label: 'Delete',
+          // Action when button is pressed
+          onPressed: () => log('Trailing action executed'),
+          label: 'Delete',
           backgroundColor: Colors.red,
         ),
       ];
@@ -128,84 +114,71 @@ class _MyHomePageState extends State<MyHomePage>
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: controller.openLeading,
-                  child: const Text('Open Leading 2'),
-                ),
-                const SizedBox(width: 8.0),
-                ElevatedButton(
-                  onPressed: controller.openTrailing,
-                  child: const Text('Open Trailing 2'),
-                ),
-                const SizedBox(width: 8.0),
-                ElevatedButton(
-                  onPressed: controller.close,
-                  child: const Text('Close 2'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            CustomSlideContextTile.withContextMenu(
-              controller: controller,
-              manager: manager,
-              leadingActions: leadingActions,
-              trailingActions: trailingActions,
-              revealAnimationType: RevealAnimationType.parallax,
-              child: const DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: ListTile(
-                  title: Text(
-                    'Swipe me Parallax',
-                    style: TextStyle(color: Colors.white),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: controller.openLeading,
+                    child: const Text('Open Leading 2'),
                   ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 8.0),
-            CustomSlideContextTile.withContextMenu(
-              manager: manager,
-              leadingActions: leadingActions,
-              trailingActions: trailingActions,
-              revealAnimationType: RevealAnimationType.pull,
-              child: const DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: ListTile(
-                  title: Text(
-                    'Swipe me Pull',
-                    style: TextStyle(color: Colors.white),
+                  const SizedBox(width: 8.0),
+                  ElevatedButton(
+                    onPressed: controller.openTrailing,
+                    child: const Text('Open Trailing 2'),
                   ),
-                ),
+                  const SizedBox(width: 8.0),
+                  ElevatedButton(
+                    onPressed: controller.close,
+                    child: const Text('Close 2'),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 8.0),
-            CustomSlideContextTile.withContextMenu(
-              manager: manager,
-              leadingActions: leadingActions,
-              // trailingActions: trailingActions,
-              revealAnimationType: RevealAnimationType.reveal,
-              child: const DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+              const SizedBox(height: 16.0),
+              CustomSlideContextTile.withContextMenu(
+                controller: controller,
+                manager: manager,
+                leadingActions: leadingActions,
+                trailingActions: trailingActions,
+                revealAnimationType: RevealAnimationType.parallax,
+                title: const Text('Swipe me Parallax'),
+                onTap: () => logAction('Parallax tapped...'),
+              ),
+              const SizedBox(height: 8.0),
+              CustomSlideContextTile.withContextMenu(
+                manager: manager,
+                leadingActions: leadingActions,
+                trailingActions: trailingActions,
+                revealAnimationType: RevealAnimationType.pull,
+                title: const Text(
+                  'Swipe me Pull',
                 ),
-                child: ListTile(
-                  title: Text(
+                subtitle: const Text('Subtitle'),
+              ),
+              const SizedBox(height: 8.0),
+              CustomSlideContextTile.withContextMenu(
+                manager: manager,
+                leadingActions: leadingActions,
+                revealAnimationType: RevealAnimationType.reveal,
+                title: const Text('Swipe me Reveal, no trailing actions'),
+              ),
+              ...List<Widget>.generate(
+                10,
+                (e) => CustomSlideContextTile.withContextMenu(
+                  manager: manager,
+                  leadingActions: leadingActions,
+                  trailingActions: trailingActions,
+                  revealAnimationType: RevealAnimationType.reveal,
+                  title: const Text(
                     'Swipe me Reveal',
-                    style: TextStyle(color: Colors.white),
                   ),
                 ),
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
